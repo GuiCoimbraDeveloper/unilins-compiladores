714e878f81e6dbd0985f010ad1182f307c2bfd3c
Guilherme Coimbra <47866067+GuiCoimbraDeveloper@users.noreply.github.com>
2020-04-06 23:33:22 -0300

    Linguagem alguma
    aprendendo sobre compilador

---
+++ algumalex/LeitorArquivosTexto.java
@@ -0,0 +1,155 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algumalex;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author guilh
+ */
+public class LeitorArquivosTexto {
+
+    private final static int TAMANHO_BUFFER = 20;
+    int[] bufferDeLeitura;
+    int ponteiro;
+    int bufferAtual;
+    int inicioLexema;
+    private String lexema;
+    InputStream is;
+
+    public LeitorArquivosTexto(String arquivo) {
+        try {
+            is = new FileInputStream(new File(arquivo));
+            inicializarBuffer();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LeitorArquivosTexto.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void inicializarBuffer() {
+        bufferAtual = 2;
+        inicioLexema = 0;
+        lexema = "";
+        bufferDeLeitura = new int[TAMANHO_BUFFER * 2];
+        ponteiro = 0;
+        recarregarBuffer1();
+    }
+
+    private void incrementrPonteiro() {
+        ponteiro++;
+        if (ponteiro == TAMANHO_BUFFER) {
+            recarregarBuffer2();
+        } else if (ponteiro == TAMANHO_BUFFER * 2) {
+            recarregarBuffer1();
+            ponteiro = 0;
+        }
+    }
+
+    private void recarregarBuffer1() {
+        if (bufferAtual == 2) {
+            bufferAtual = 1;
+
+            for (int i = 0; i < TAMANHO_BUFFER; i++) {
+                try {
+                    bufferDeLeitura[i] = is.read();
+                    if (bufferDeLeitura[i] == -1) {
+                        break;
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(LeitorArquivosTexto.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    private void recarregarBuffer2() {
+        if (bufferAtual == 1) {
+            bufferAtual = 2;
+
+            for (int i = TAMANHO_BUFFER; i < TAMANHO_BUFFER * 2; i++) {
+                try {
+                    bufferDeLeitura[i] = is.read();
+                    if (bufferDeLeitura[i] == -1) {
+                        break;
+                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(LeitorArquivosTexto.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    private int lerCaractereDBuffer() {
+        int ret = bufferDeLeitura[ponteiro];
+        System.out.println(this);
+        incrementrPonteiro();
+        return ret;
+    }
+
+    public int lerProximoCaractere() {
+        int ret = lerCaractereDBuffer();
+        lexema += (char) ret;
+        return ret;
+    }
+
+    public void retroceder() {
+        ponteiro--;
+        lexema = lexema.substring(0, lexema.length() - 1);
+        if (ponteiro < 0) {
+            ponteiro = TAMANHO_BUFFER * 2 - 1;
+        }
+    }
+
+    public void zerar() {
+        ponteiro = inicioLexema;
+        lexema = "";
+    }
+
+    public void confirmar() {
+        inicioLexema = ponteiro;
+        lexema = "";
+    }
+
+    public String getLexema() {
+        return lexema;
+    }
+
+    @Override
+    public String toString() {
+        String ret = "Buffer:[";
+        for (int i : bufferDeLeitura) {
+            char c = (char) i;
+            if (Character.isWhitespace(c)) {
+                ret += ' ';
+            } else {
+                ret += (char) i;
+            }
+        }
+        ret += "]\n";
+        ret += "         ";
+        for (int i = 0; i < TAMANHO_BUFFER * 2; i++) {
+            if (i == inicioLexema && i == ponteiro) {
+                ret += "%";
+            } else if (i == inicioLexema) {
+                ret += "^";
+            } else if (i == ponteiro) {
+                ret += "*";
+            } else {
+                ret += " ";
+            }
+        }
+
+        return ret;
+    }
+
+}
---
+++ algumalex/TipoToken.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algumalex;
+
+/**
+ *
+ * @author guilh
+ */
+public enum TipoToken {
+    PCDeclaracoes ,PCAlgoritmo, PCInteiro, PCReal,PCAtribuir,PCA,PCLer,
+    PCImprimir,PCSe,PCEntao,PCEnquanto,PCInicio,PCFim,
+    OpAritMult,OpAritDiv,OpAritSoma,OpAritSub,
+    OpRelMenor,OpRelMenorIgual,OpRelMaiorIgul,
+    OpRelMaior,OpRelIgual,OpRelDif,
+    OpBoolE,OpBoolOu,
+    Delim,AbrePar,FechaPar,
+    Var,NumInt,NumReal,Cadeia,Fim
+}
