0b6301fc15e9f4f7081f1352b6f37938a34fb9a4
Guilherme Coimbra <47866067+GuiCoimbraDeveloper@users.noreply.github.com>
2020-05-12 21:08:13 -0300

    Gerador de codigo da linguagem Alguma
    Tem um extra para linguagem de maquina usando código P

---
+++ alguma-gerador/src/main/java/com/gui/compiladores/alguma/gerador/Principal.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gui.compiladores.alguma.gerador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+/**
+ *
+ * @author guilh
+ */
+public class Principal {
+
+    public static void main(String args[]) throws IOException {
+        CharStream cs = CharStreams.fromFileName(args[0]);
+        AlgumaLexer lexer = new AlgumaLexer(cs);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        AlgumaParser parser = new AlgumaParser(tokens);
+        AlgumaParser.ProgramaContext arvore = parser.programa();
+        AlgumaSemantico as = new AlgumaSemantico();
+        as.visitPrograma(arvore);
+        AlgumaSemanticoUtils.errosSemanticos.forEach((s) -> System.out.println(s));
+
+        if (AlgumaSemanticoUtils.errosSemanticos.isEmpty()) {
+            AlgumaGeradorC agc = new AlgumaGeradorC();
+            agc.visitPrograma(arvore);
+            try (PrintWriter pw = new PrintWriter(args[1])) {
+                pw.print(agc.saida.toString());
+            }
+
+            AlgumaGeradorPCodigo agp = new AlgumaGeradorPCodigo();
+            String pcod = agp.visitPrograma(arvore);
+            try (PrintWriter pw = new PrintWriter(args[2])) {
+                pw.print(pcod);
+            }
+        }
+    }
+}
